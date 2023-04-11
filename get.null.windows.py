b58f3d24912b53aa72d9e7090c9f2d33cc0d4586
mheskett <mbheskett@gmail.com>
2023-04-11 12:34:06 -0700

    snps

--- get.null.windows.py
+++ get.null.windows.py
@@ -16,8 +16,10 @@ import seaborn as sns
 
 ### all files needed
 fasta_fai = "/Users/heskett/breast.fragile.sites/reference_files/genome.fa.fai"
-blacklist = "/Users/heskett/breast.fragile.sites/reference_files/hg19-blacklist.v2.bed"
+blacklist_file = "/Users/heskett/breast.fragile.sites/reference_files/hg19-blacklist.v2.bed"
 genome_fasta = "/Users/heskett/breast.fragile.sites/reference_files/genome.fa"
+common_snps = "/Users/heskett/breast.fragile.sites/reference_files/ucsc.common.snps.nochr.bed"
+
 ###
 
 
@@ -28,7 +30,7 @@ def make_windows(length, overlap_fraction=0.25, write_file=False, file_path=None
     return windows
 
 
-def sample_windows(number,window_object,fraction_windows=None,window_file=None):
+def sample_windows(number, window_object, fraction_windows=None, window_file=None):
     if number > len(window_object):
         print("ERROR: trying to sample more windows than available")
         return
@@ -38,35 +40,57 @@ def sample_windows(number,window_object,fraction_windows=None,window_file=None):
 
 def random_windows(length,number, write_file=False, file_path=None):
     a=pybedtools.BedTool()
-    windows = a.random(l=length, n=number, genome=fasta_fai)
+    windows = a.random(l=length, n=number, g=fasta_fai)
 
     return windows
 
 
 def remove_blacklist(windows):
-    blacklist = pybedtools.BedTool(blacklist)
-    windows = windows.subtract(blacklist,A=True)
+    blacklist = pybedtools.BedTool(blacklist_file)
+    windows = windows.subtract(blacklist, A=True)
 
     return windows
 
 def calculate_gc(windows):
-    ### names=["#1_usercol", "2_usercol", "3_usercol",  "4_pct_at",  "5_pct_gc",  "6_num_A",    "7_num_C",  "8_num_G",  "9_num_T",   "10_num_N",  "11_num_oth",  "12_seq_len"]
+    # can either chop at chrom start stop, or keep everything and then create the header basd on how long
+    # names=["#1_usercol", "2_usercol", "3_usercol",  "4_pct_at",  "5_pct_gc",  "6_num_A",    "7_num_C",  "8_num_G",  "9_num_T",   "10_num_N",  "11_num_oth",  "12_seq_len"],
+    num_cols = len(windows.to_dataframe().columns)
     windows_nuc = windows.nucleotide_content(fi=genome_fasta)
-    windows_nuc_df = windows_nuc.to_dataframe(names=["#1_usercol", "2_usercol", "3_usercol",  "4_pct_at",  "5_pct_gc",  "6_num_A",    "7_num_C",  "8_num_G",  "9_num_T",   "10_num_N",  "11_num_oth",  "12_seq_len"],
+    windows_nuc_df = windows_nuc.to_dataframe(
 
-        disable_auto_names=True,header=None).drop(index=0)
+        disable_auto_names=True)
     print(windows_nuc_df)
     plt.figure()
-    sns.kdeplot([float(x) for x in windows_nuc_df["5_pct_gc"]],clip=(0,1))
-    plt.xlim([0.2,0.91])
-    plt.xticks([0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9])
-    plt.savefig("test.pdf")
+    sns.kdeplot([float(x) for x in windows_nuc_df[str(num_cols+2)+"_pct_gc"]], clip=(0, 1))
+    plt.xlim([0,1])
+    plt.xticks([0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1])
+    plt.savefig("gc.pdf")
+
+    return
+
+def common_snp_density(windows):
+    num_cols = len(windows.to_dataframe().columns)
+
+    snps = pybedtools.BedTool(common_snps)
+    window_snps_df = windows.intersect(snps,c=True).to_dataframe(disable_auto_names=True,header=None)
+    print(window_snps_df)
+    window_snps_df["snp_density"] = window_snps_df[num_cols].astype(int) / ((window_snps_df[2].astype(int) - window_snps_df[1].astype(int)) / 1000)
+    
+    plt.figure()
+    sns.kdeplot(window_snps_df["snp_density"], clip=(0, 10),label="snps_per_kb")
+    plt.xlim([0,10])
+    plt.xticks(list(range(0,11)))
+    plt.suptitle("snps_per_kb")
+    plt.savefig("snps_per_kb.pdf")
+
     return
 
 
-calculate_gc(pybedtools.BedTool("/Users/heskett/breast.fragile.sites/reference_files/test.bed"))
+#calculate_gc(remove_blacklist(random_windows(100000,1000))) # dont forget random seeds
+
+#calculate_gc(pybedtools.BedTool("/Users/heskett/breast.fragile.sites/reference_files/test.bed"))
 
 
-
+common_snp_density(random_windows(20000,500))
 
 
