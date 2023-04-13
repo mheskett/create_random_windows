bd38e9e85f47242df55882167c892780a0dc3bee
mheskett <mbheskett@gmail.com>
2023-04-12 17:27:36 -0700

    command lines

--- get.null.windows.py
+++ get.null.windows.py
@@ -12,39 +12,23 @@ import multiprocessing as mp
 import random
 import json
 import seaborn as sns
-
+import argparse
 
 ### all files needed
 fasta_fai = "/Users/heskett/breast.fragile.sites/reference_files/genome.fa.fai"
-blacklist_file = "/Users/heskett/breast.fragile.sites/reference_files/hg19-blacklist.v2.bed"
+blacklist_file = "/Users/heskett/breast.fragile.sites/reference_files/hg19-blacklist.v2.nochr.bed"
 genome_fasta = "/Users/heskett/breast.fragile.sites/reference_files/genome.fa"
 common_snps = "/Users/heskett/breast.fragile.sites/reference_files/ucsc.common.snps.nochr.bed"
 repeats_file = "/Users/heskett/breast.fragile.sites/reference_files/ucsc.repeats.hg19.nochr.bed"
 dead_zones_file = "/Users/heskett/breast.fragile.sites/reference_files/ucsc.ncbi.dead.zones.nochr.bed"
 problematic_regions_file = "/Users/heskett/breast.fragile.sites/reference_files/ucsc.problematic.nochr.bed"
+whole_genes_file = "/Users/heskett/breast.fragile.sites/reference_files/ucsc.ensemble.coding.whole.genes.bed" ### includes introns and exons of coding genes only
 ###
-
-
-def make_windows(length, overlap_fraction=0.25, write_file=False, file_path=None):
-    a=pybedtools.BedTool()
-    windows=a.window_maker(g=fasta_fai, w=length, s=length*window_fraction)
-
-    return windows
-
-
-def sample_windows(number, window_object, fraction_windows=None, window_file=None):
-    if number > len(window_object):
-        print("ERROR: trying to sample more windows than available")
-        return
-
-    return 
-
 
 def random_windows(length,number, write_file=False, file_path=None):
     a=pybedtools.BedTool()
     windows = a.random(l=length, n=number, g=fasta_fai)
     ## this returns (chr start stop number index, length, random strand.)
-    print(windows)
 
     return windows
 
@@ -54,32 +38,33 @@ def remove_blacklist(windows):
     problematic_regions = pybedtools.BedTool(problematic_regions_file)
     dead_zones = pybedtools.BedTool(dead_zones_file)
     windows = windows.subtract(blacklist, A=True).subtract(problematic_regions,A=True).subtract(dead_zones,A=True)
-    print(windows)
 
     return windows
+
 
 def calculate_gc(windows):
     # can either chop at chrom start stop, or keep everything and then create the header basd on how long
     # names=["#1_usercol", "2_usercol", "3_usercol",  "4_pct_at",  "5_pct_gc",  "6_num_A",    "7_num_C",  "8_num_G",  "9_num_T",   "10_num_N",  "11_num_oth",  "12_seq_len"],
     num_cols = len(windows.to_dataframe().columns)
     windows_nuc = windows.nucleotide_content(fi=genome_fasta)
-    windows_nuc_df = windows_nuc.to_dataframe(
-
-        disable_auto_names=True)
+    windows_nuc_df = windows_nuc.to_dataframe(disable_auto_names=True)
     print(windows_nuc_df)
     plt.figure()
     sns.kdeplot([float(x) for x in windows_nuc_df[str(num_cols+2)+"_pct_gc"]], clip=(0, 1))
     plt.xlim([0,1])
+    plt.suptitle("gc fraction")
     plt.xticks([0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1])
     plt.savefig("gc.pdf")
 
-    return
+    return windows_nuc
+
 
 def common_snp_density(windows):
     num_cols = len(windows.to_dataframe().columns)
 
     snps = pybedtools.BedTool(common_snps)
-    window_snps_df = windows.intersect(snps,c=True).to_dataframe(disable_auto_names=True,header=None)
+    windows_snps = windows.intersect(snps,c=True)
+    window_snps_df = windows_snps.to_dataframe(disable_auto_names=True,header=None)
     print(window_snps_df)
     window_snps_df["snp_density"] = window_snps_df[num_cols].astype(int) / ((window_snps_df[2].astype(int) - window_snps_df[1].astype(int)) / 1000)
     
@@ -90,24 +75,133 @@ def common_snp_density(windows):
     plt.suptitle("snps_per_kb")
     plt.savefig("snps_per_kb.pdf")
 
-    return
+    return windows_snps
 
 
 def fraction_repeats(windows):
     repeats = pybedtools.BedTool(repeats_file)
-    windows_repeats_df = windows.coverage(repeats).to_dataframe(disable_auto_names=True,header=None)
-    last_col=len(windows_repeats_df)-1
+    windows_repeats = windows.coverage(repeats)
+    windows_repeats_df = windows_repeats.to_dataframe(disable_auto_names=True,header=None)
+    last_col=len(windows_repeats_df.columns)-1
     print(windows_repeats_df)
     plt.figure()
-    sns.kdeplot(windows_repeats_df[9], clip=(0, 1),label="fraction_repeats")
+    sns.kdeplot(windows_repeats_df[last_col], clip=(0, 1),label="fraction_repeats")
     plt.xlim([0,1])
     plt.xticks([0,0.2,0.4,0.6,0.8,1])
     plt.suptitle("Fraction_Repeat_Sequence")
     plt.savefig("fraction_Repeat_sequence.pdf")
+
+    return windows_repeats
+
+
+def fraction_coding(windows):
+    whole_genes = pybedtools.BedTool(whole_genes_file)
+    windows_genes = windows.coverage(whole_genes)
+    windows_genes_df = windows_genes.to_dataframe(disable_auto_names=True,header=None)
+    
+    last_col=len(windows_genes_df.columns)-1
+    print(windows_genes_df)
+
+    plt.figure()
+    sns.kdeplot(windows_genes_df[last_col], clip=(0, 1),label="fraction_repeats")
+    plt.xlim([0,1])
+    plt.xticks([0,0.2,0.4,0.6,0.8,1])
+    plt.suptitle("Fraction_whole_gene_Sequence")
+    plt.savefig("fraction_genes_sequence.pdf")
+
     return
 
-random_windows(100000,1000) # dont forget random seed
-#calculate_gc(pybedtools.BedTool("/Users/heskett/breast.fragile.sites/reference_files/test.bed"))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="make windows")
+
+    parser.add_argument("--bed",
+       type=str,
+       metavar="[bed file input. no header.]",
+       required=False,
+       help="")
+    parser.add_argument("--out_directory",
+       type=str,
+       metavar="[out directory]",
+       required=True,
+       help="full path to output results")
+    parser.add_argument("--make_windows",
+       type=str,
+       metavar="[make windows or no]",
+       required=False,
+       help="")
+    parser.add_argument("--gc_min",
+       type=float,
+       metavar="[min fraction of gc]",
+       required=False,
+       help="")
+    parser.add_argument("--gc_max",
+       type=float,
+       metavar="[max fraction of gc]",
+       required=False,
+       help="")
+    parser.add_argument("--repeats_min",
+       type=float,
+       metavar="[min fraction of repeats]",
+       required=False,
+       help="")
+    parser.add_argument("--repeats_max",
+       type=float,
+       metavar="[max fraction of repeats]",
+       required=False,
+       help="")
+    parser.add_argument("--gene_fraction_min",
+       type=float,
+       metavar="[min fractino of genes in windows allowed]",
+       required=False,
+       help="")
+    parser.add_argument("--gene_fraction_max",
+       type=float,
+       metavar="[max fraction of genes in windows allowed]",
+       required=False,
+       help="")
+    parser.add_argument("--num_windows",
+       type=int,
+       metavar="[number of windows wanted]",
+       required=False,
+       help="")
+    parser.add_argument("--length_windows",
+       type=int,
+       metavar="[length of windows wanted]",
+       required=False,
+       help="")   
+
+    arguments = parser.parse_args()
+
+
+    if arguments.bed:
+        input_file = pybedtools.BedTool(arguments.bed)
+        # return all the stats
+        fraction_coding(input_file)
+        fraction_repeats(input_file)
+        common_snp_density(input_file)
+        calculate_gc(input_file)
+    else:
+
+        my_windows = remove_blacklist(random_windows(length=arguments.length_windows,number=arguments.num_windows*2))
+        print(my_windows.to_dataframe(disable_auto_names=True,header=None).sample(n=arguments.num_windows))
+
+
+
+
+
+
+
+
+
+
+# common_snp_density(fraction_repeats(calculate_gc(remove_blacklist(random_windows(10000,1000))))) # dont forget random seed)
 #common_snp_density(random_windows(20000,500))
 #fraction_repeats(random_windows(20000,500))
+#fraction_coding(random_windows(50000,5000))
+
+## main program should get stats for a list of windows and then allow you to put in similar stats to make new windows
+
+
+
 
